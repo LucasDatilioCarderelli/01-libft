77fb71f799009045c3cb0acb939b7d0a9874b4ab
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2021-09-12 23:13:11 -0300

    bonus done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:34:02 by ldatilio          #+#    #+#             */
-/*   Updated: 2021/09/11 21:58:44 by ldatilio         ###   ########.fr       */
+/*   Updated: 2021/09/12 23:12:13 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_llst;
-	
-	return (new_llst);
-}
+	t_list	*new_llist;
+	t_list	*new_element;
 
+	if (!lst)
+		return (NULL);
+	new_llist = NULL;
+	while (lst)
+	{
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
+		{
+			ft_lstclear(&new_element, del);
+			return (NULL);
+		}
+		else
+			ft_lstadd_back(&new_llist, new_element);
+		lst = lst -> next;
+	}
+	return (new_llist);
+}
