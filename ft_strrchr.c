ea9110c8a403253622abdec02bf4cfa74822fc51
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2021-09-16 13:47:22 -0300

    add stop

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:47:49 by ldatilio          #+#    #+#             */
-/*   Updated: 2021/09/16 13:31:16 by ldatilio         ###   ########.fr       */
+/*   Updated: 2021/09/16 13:46:03 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 char	*ft_strrchr(const char *str, int c)
 {
 	int			len;
-	const char	*ini;
 
-	ini = &str[0];
 	len = ft_strlen(str);
-	while (&str[len] != ini && str[len] != c)
+	while (str[len] != c && len >= 0)
 		len--;
 	if (str[len] == c)
 		return (&((char *)str)[len]);
